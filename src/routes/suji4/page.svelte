62c0a88a2c66322304c5052f9bfac6713b6d0b8a
yamagumo <131755296+yamagumo@users.noreply.github.com>
2024-09-15 01:40:57 +0900

    page4作成

---
+++ src/routes/suji4/page.svelte
@@ -0,0 +1,62 @@
+<div class="wrapper">
+    <h1 class="takuann">Suji</h1>
+      <div class="container-fluid p-5 my-5  text-white">
+        <h1 style="text-align: center;" class="bg-danger">ToDoを追加</h1>
+      </div>
+      <div class="kyc-search-bar" style="text-align: center;">
+        <input class="kyc-search-box" type="text" placeholder="検索" autocomplete="off">
+    </div>
+      <div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
+        <button type="button" class="btn btn-outline-primary">💪</button>
+        <button type="button" class="btn btn-outline-primary">＋</button>
+        <button type="button" class="btn btn-outline-primary">✏️</button>
+      </div>
+    </div>
+    <style>
+      .kyc-search-bar{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 45px;
+    width: 550px;
+    border-radius: 30px;
+    margin: 5px;
+    border: solid 1px #dcdcdc;
+}
+    
+.kyc-search-box{
+    font-size: 16px;
+    width: 100%;
+    border: none;
+    outline: none;
+}
+
+i.kyc-search-mark{
+    margin-left: 10px;
+    margin-right: 5px;
+    color: #9aa0a6;
+}
+
+i.kyc-search-mic{
+    margin-right: 10px;
+    cursor: pointer;
+    color: #9aa0a6;
+}
+
+@media only screen and (max-width:480px){
+    .kyc-search-bar{
+        width: 90%;
+    }
+}
+
+      .takuann{
+        text-align: center;
+        color: red;
+          }
+      .wrapper{
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        min-height: 100svh;
+          }
+    
+    </style>
