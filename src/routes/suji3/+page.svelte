2ec6b1bfc0120a51490047bd46ae1344714d0dae
yamagumo <131755296+yamagumo@users.noreply.github.com>
2024-09-15 01:39:44 +0900

    page3作成

---
+++ src/routes/suji3/+page.svelte
@@ -0,0 +1,23 @@
+<div class="wrapper">
+    <h1 class="takuann">Suji</h1>
+      <div class="container-fluid p-5 my-5  text-white">
+        <h1 style="text-align: center;" class="bg-danger">勉強をする</h1>
+      </div>
+      <div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
+        <button type="button" class="btn btn-outline-primary">💪</button>
+        <button type="button" class="btn btn-outline-primary">＋</button>
+        <button type="button" class="btn btn-outline-primary">✏️</button>
+      </div>
+    </div>
+    <style>
+      .takuann{
+        text-align: center;
+        color: red;
+          }
+      .wrapper{
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        min-height: 100svh;
+          }
+    
+    </style>
